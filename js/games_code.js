1fec345085b87ac6092a51b182907e3ce6d6aee7
raysan5 <raysan5@gmail.com>
2016-07-18 12:56:00 +0200

    Adder users games

--- js/games_code.js
+++ js/games_code.js
@@ -21,7 +21,13 @@ $(document).ready(function() {
         'game_just_do',
         'game_skully_escape',
         'game_koala_seasons',
-        'game_light_my_ritual'];
+        'game_light_my_ritual',
+        'user_raymario',    
+        'user_crazyball',
+        'user_tictactoe',
+        'user_super_red_rope',
+        'user_taptojamp',    
+        'user_funwithsat'];
 
     var exampleDesc = [
         'arkanoid',
@@ -41,20 +47,43 @@ $(document).ready(function() {
         'JUST DO',
         'Skully Escape',
         'Koala Seasons',
-        'LIGHT MY RITUAL'];
+        'LIGHT MY RITUAL',
+        'RayMario (Victor Fisac)',    
+        'CrazyBall (Victor Fisac)',
+        'Tic-Tac-Toe (LelixSuper)',
+        'Super Red Rope Puzzles (Kurnic)',
+        'TapToJAmp (MarcMDE)',    
+        'FunWithSATCollisions (MarcMDE)'];
 
     for (var i = 0; i < exampleName.length; i++)
     {
         var filterType = exampleName[i].substring(0, exampleName[i].indexOf("_"));
         
-        // TODO: samples open an exmain.html samples page while full games open a brand new page
+        var linkTag;
         
-        var linkTag
+        if (filterType == 'sample') linkTag = '<a class="fancybox fancybox.iframe" href="games/sample_main.html?name=' + exampleName[i] + '" title="' + exampleDesc[i] + '">';
+        else if (filterType == 'game') linkTag = '<a target="_blank" href="games/' + exampleName[i].substring(5) + '.html" title="' + exampleDesc[i] + '">';
+        else if (filterType == 'user')
+        {
+            switch (exampleName[i])
+            {
+                case 'user_raymario': linkTag = '<a target="_blank" href="https://github.com/victorfisac/RayMario" title="' + exampleDesc[i] + '">'; break;
+                case 'user_crazyball': linkTag = '<a target="_blank" href="https://github.com/victorfisac/CrazyBall" title="' + exampleDesc[i] + '">'; break;
+                case 'user_tictactoe': linkTag = '<a target="_blank" href="https://github.com/LelixSuper/TicTacToe" title="' + exampleDesc[i] + '">'; break;
+                case 'user_super_red_rope': linkTag = '<a target="_blank" href="https://kurnic.itch.io/super-red-rope-puzzles" title="' + exampleDesc[i] + '">'; break;
+                case 'user_taptojamp': linkTag = '<a target="_blank" href="http://marcmde.github.io/TapToJAmp_v2_0/" title="' + exampleDesc[i] + '">'; break;
+                case 'user_funwithsat': linkTag = '<a target="_blank" href="http://marcmde.github.io/FunWithSATCollisions_v_1_0_Browser/" title="' + exampleDesc[i] + '">'; break;
+                default: break;
+            }
+            /*
+            https://github.com/MarcMDE/TapToJump    //http://marcmde.github.io/TapToJump_v1_0_Browser/
+            https://github.com/MarcMDE/TapToJAmp    //http://marcmde.github.io/TapToJAmp_v2_0/
+            https://github.com/MarcMDE/FunWithSATCollisions //http://marcmde.github.io/FunWithSATCollisions_v_1_0_Browser/
+            https://kurnic.wordpress.com/portfolio/flappy-jetpack/
+            */
+        }
         
-        if (filterType == 'sample') linkTag = '<a class="fancybox fancybox.iframe" href="games/sample_main.html?name=' + exampleName[i] + '" title="' + exampleDesc[i] + '">'
-        else if (filterType == 'game') linkTag = '<a target="_blank" href="games/' + exampleName[i].substring(5) + '.html" title="' + exampleDesc[i] + '">'
-        
-        if (exampleName[i] == 'game_koala_seasons') linkTag = '<a target="_blank" href="http://www.koalaseasons.com" title="' + exampleDesc[i] + '">'
+        if (exampleName[i] == 'game_koala_seasons') linkTag = '<a target="_blank" href="http://www.koalaseasons.com" title="' + exampleDesc[i] + '">';
 
         $('#container').append(
             '<div class="mix f' + filterType + '">' + linkTag +
